be25e4a8739729db8e66b727c18745cb913f322c
test <211250245@smail.nju.edu.cn>
2025-11-17 16:40:00 +0800

    收起了docker配置文件, 引进了小型测试脚本

---
+++ scripts/train_adapted.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+python train.py data/large_gensfen_multipvdiff_100_d9.binpack \
+    --gpus 0 \
+    --max_epochs 1 \
+    --batch-size 16 \
+    --epoch-size 100 \
+    --validation-size 20 \
+    --num-workers 1 \
+    --threads 1 \
+    --seed 42 \
+    --no-smart-fen-skipping \
+    --no-wld-fen-skipping \
+    --random-fen-skipping 0 \
+    --features "HalfKAv2_hm^" \
+    --lambda 1.0 \
+    --lr 0.001 \
+    --default_root_dir "./logs_small_test"
